25df6983fd280f841e0ef440b8ff5f6e9d268004
satyamkhadka <satyamkhadka@krennovatech.com>
2019-05-15 14:28:53 +0545

    update

--- api_project.js
+++ api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-05-15T08:19:24.935Z",
+    "time": "2019-05-15T08:42:27.067Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
